06cb371e336fc60167bc22ee5b71737eb464f1b5
freedomcry <90600100+ZhandosBaltabaev@users.noreply.github.com>
2023-10-27 10:51:03 +0600

    Add files via upload

--- practice 3.dart
+++ practice 3.dart
@@ -1,36 +1,36 @@
 void performOperation(int number1, int number2, String operation) {
-  double result;
+double result;
 
-  switch (operation) {
-    case '+':
-      result = number1 + number2;
-      break;
-    case '-':
-      result = number1 - number2;
-      break;
-    case '*':
-      result = number1 * number2;
-      break;
-    case '/':
-      if (number2 != 0) {
-        result = number1 / number2;
-      } else {
-        print('Деление на ноль невозможно.');
-        return;
-      }
-      break;
-    default:
-      print('Недопустимая операция: $operation');
-      return;
-  }
+switch (operation) {
+case '+':
+result = (number1 + number2).toDouble();
+break;
+case '-':
+result = (number1 - number2).toDouble();
+break;
+case '*':
+result = (number1 * number2).toDouble();
+break;
+case '/':
+if (number2 != 0) {
+result = number1 / number2;
+} else {
+print('Деление на ноль невозможно.');
+return;
+}
+break;
+default:
+print('Недопустимая операция: $operation');
+return;
+}
 
-  print('$number1 $operation $number2 = $result');
+print('$number1 $operation $number2 = $result');
 }
 
 void main() {
-  int num1 = 5;
-  int num2 = 19;
-  String operation = '-'; // Здесь можно заменить символ на '+', '*', или '/'
+int num1 = 5;
+int num2 = 19;
+String operation = '-'; // Здесь можно заменить символ на '+', '*', или '/'
 
-  performOperation(num1, num2, operation);
+performOperation(num1, num2, operation);
 }
